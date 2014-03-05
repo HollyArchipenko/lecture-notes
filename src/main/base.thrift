5b5470df2bf065cc9ad74be5a25be008d8e5c546
anastasiaprivalova <anastasia.privalova23@gmail.com>
2014-03-05 17:21:13 +0200

    О

--- src/main/base.thrift
+++ src/main/base.thrift
@@ -1,16 +1,26 @@
 namespace java org.lecturenotes
 namespace js org.lecturenotes
 
-struct Subscribtion {
-	1: string userID
-	2: string lectureID
+
+struct Subscribtion{
+   1: string UserId
+   2: string LectureId
 }
 
-struct Lecture {
-	1: string id
+struct Lecture{
+	1: i32 Id
 	2: string name
 	3: i16 course
-	4: i64 time //дата и время
+	4: i64 time
 }
 
-typedef list<Lecture> lecturesList
+struct Subject{
+	1: string name
+	2: i16 course
+	3: SEMESTER semester
+}
+
+enum SEMESTER{
+     AUTUMN
+     SPRING
+}
