24828212db6f5e3d915d6c7690b621c275380961
HollyArchipenko <Holly.Archipenko@gmail.com>
2014-04-02 16:13:24 +0300

    Update service.thrift

--- src/main/service.thrift
+++ src/main/service.thrift
@@ -1,4 +1,24 @@
-service Subscription {
-	string get(1: string userID, 2: string lectureID)
-	void put(1: string userID, 2: string lectureID)
+include 'base.thrift'
+
+service Subscript { 
+    bool subscript(1: string userID, 2: Course lecture)
+    bool unsubscript(1: string userID, 2: Course lecture)
+}
+
+service LectureNotes {
+    string uploadLecture (1: Lecture lecture)
+    bool removeLecture (1: Lecture lecture)
+    Lecture downloadLecture (1: Course lectureId)
+}
+
+service GetSubscriptions {
+    MissedLecture fetchAll(1: string userId)
+    MissedLecture fetchUnviewed(1: string userId)
+}
+
+service User {
+    string addNewUser (1: string name, 2: string email, 3: string password)
+    User getProfile (1: string id)
+    string changeProfile (1: string id, 2: changeUser data)
+    SessionHash signIn (1: string name, 2: string password)
 }
